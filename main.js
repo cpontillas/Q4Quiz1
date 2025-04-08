8c8e1d4b72b45108067dfc77a4c3559bbbfe3f0e
cpontillas <pontillas.cv@obmontessori.edu.ph>
2025-04-08 08:33:17 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+    function calculateBMI() {
+
+      const weight = parseFloat(document.getElementById("weight").value);
+      const height = parseFloat(document.getElementById("height").value);
+      const output = document.getElementById("output");
+
+      if (weight > 0 && height > 0) {
+
+        const bmi = weight / (height * height);
+        let category = "";
+
+        if (bmi < 18.5) {
+
+          category = "Underweight";
+
+        }
+
+        else if (bmi >= 18.5 && bmi <= 24.9) {
+
+          category = "Normal weight";
+
+        }
+
+        else if (bmi >= 25 && bmi <= 29.9) {
+
+          category = "Overweight";
+        }
+
+        else if (bmi >= 30) {
+
+          category = "Obese";
+        }
+
+        output.innerText = `Your BMI is: ${bmi.toFixed(2)}\nYou are ${category}`;
+
+      }
+
+      else {
+
+        output.innerText = "Please enter valid values!";
+
+      }
+
+    }
